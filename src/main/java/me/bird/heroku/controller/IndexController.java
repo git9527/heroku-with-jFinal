b321f60964b9fd6fa8be23f1c126ec7872e24201
zhangsn <zhangsn@travelsky.laptop>
2014-05-15 18:34:37 +0800

    update time display

--- src/main/java/me/bird/heroku/controller/IndexController.java
+++ src/main/java/me/bird/heroku/controller/IndexController.java
@@ -1,6 +1,7 @@
 package me.bird.heroku.controller;
 
 import me.bird.heroku.consts.BaseConsts;
+import me.bird.heroku.utils.DateUtil;
 
 import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
@@ -9,6 +10,6 @@ public class IndexController extends Controller{
 
 	@ActionKey("/")
 	public void index(){
-		super.renderHtml("启动时间:" + BaseConsts.START_TIME);
+		super.renderHtml("启动时间:" + DateUtil.toString(BaseConsts.START_TIME, DateUtil.yyyy_MM_dd_HH_mm_ss));
 	}
 }
